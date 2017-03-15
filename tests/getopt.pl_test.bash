93a83ce5363966bea7b13216273b70d427e2af2b
Makoto Onuki <makoto.onuki@gmail.com>
2017-03-14 21:50:32 -0700

    Adding tests to getopt.pl

--- tests/getopt.pl_test.bash
+++ tests/getopt.pl_test.bash
@@ -2,11 +2,69 @@
 
 . testutil.bash
 
-flag1=0
-eval "$(getopt.pl -xu usage '
-a   flag1=1 # comment
-' "$@")"
+#-----------------------------------
+flag=0
+eval "$(getopt.pl '
+a   flag=1 # comment
+')"
 
-assert '(( $flag1 == 0 ))'
-assert '(( $# == 0 ))'
+assert "(( $flag == 0 ))"
+assert "[[ '$*' == '' ]]"
 
+#-----------------------------------
+flag=0
+eval "$(getopt.pl '
+a   flag=1 # comment
+' -a)"
+
+assert "(( $flag == 1 ))"
+assert "[[ '$*' == '' ]]"
+
+#-----------------------------------
+flag=0
+long=0
+eval "$(getopt.pl '
+a        flag=1 # comment
+b|long   long=1 # comment
+' --long a  'b  c'  def)"
+
+assert "(( $flag == 0 ))"
+assert "(( $long == 1 ))"
+assert "(( $# == 3 ))"
+assert "[[ '$2' == 'b  c' ]]"
+assert "[[ '$*' == 'a b  c def' ]]"
+
+#-----------------------------------
+flag=0
+long=0
+
+actual() {
+  eval "$(getopt.pl '
+  a        flag=1 # comment1
+  b|long   long=1 # comment2
+  c=s      long=1 # comment3
+  long=s   long=1 # more flag
+  d:       long=1 # even more flag
+  ' -h)"
+}
+
+assert_out actual <<'EOF'
+
+  actual:
+
+  Usage:
+    -a
+                comment1
+    -b --long
+                comment2
+    -c=s
+                comment3
+    --long=s
+                more flag
+    -d=s
+                even more flag
+    -h --help
+                Show this help.
+    --bash-completion
+                Print bash completion script.
+EOF
---
+++ testutil.bash
@@ -0,0 +1,72 @@
+
+set -e
+
+declare -i _num_successes=0
+declare -i _num_failures=0
+declare -i _need_newline_before_failure=0
+
+_at_exit() {
+  echo ""
+  echo "----"
+  echo "$(( $_num_successes + $_num_failures )) tests executed, $_num_successes passed, $_num_failures failed." 1>&2
+  (( $_num_failures == 0 && $_num_successes > 0 ))
+  exit $?
+}
+
+succeed() {
+  _num_successes=$(( $_num_successes + 1 ))
+  echo -n "."
+  _need_newline_before_failure=1
+}
+
+fail() {
+  if (( $_need_newline_before_failure )) ; then
+    echo
+  fi
+  _num_failures=$(( $_num_failures + 1 ))
+  _need_newline_before_failure=0
+}
+
+# eval the entire arguments and check the status code.
+assert() {
+  local exp="$*"
+
+  set +e
+  eval "$exp"
+  local rc=$?
+  set -e
+  if (( $rc == 0 )) ; then
+    succeed
+    return 0
+  fi
+  fail
+  {
+    echo "- Test '$exp' failed at:"
+    caller | sed -e 's/^/    /'
+  } 1>&2
+  return 0
+}
+
+assert_out() {
+  local cmd="$1"
+  set +e
+  out=$(wdiff -n \
+      -w $'\033[30;41m' -x $'\033[0m' \
+      -y $'\033[30;42m' -z $'\033[0m' \
+      <(eval '$cmd' || true) <(cat))
+  local rc=$?
+  set -e
+  if (( $rc == 0 )) ; then
+    succeed
+  else
+    {
+      echo "- Test '$exp' failed at:"
+      caller | sed -e 's/^/    /'
+      echo "diff was:"
+      echo "$out"
+    } 1>&2
+    fail
+  fi
+}
+
+trap _at_exit EXIT
