123eb091e2130d616712ea48247d53bc23576180
Makoto Onuki <makoto.onuki@gmail.com>
2017-07-29 19:47:24 -0700

    latest

--- Getopt.pm
+++ Getopt.pm
@@ -1,3 +1,12 @@
-use strict;
-use Getopt::Long qw(:config gnu_compat bundling require_order);
-use Data::Dumper;
+# use strict;
+# use Getopt::Long qw(:config gnu_compat bundling require_order);
+
+# sub getopt {
+#   my @options = @_;
+
+#   push @options, "--bash-completion", "Print bash completion script.";
+
+#   my
+
+
+# }
