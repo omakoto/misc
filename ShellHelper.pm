f1d13b535603f6c28dfcbac935bef38e0f090c22
Makoto Onuki <makoto.onuki@gmail.com>
2017-08-19 10:51:12 -0700

    Started working on shellhelper.

--- ShellHelper.pm
+++ ShellHelper.pm
@@ -1,21 +1,44 @@
+package ShellHelper;
+
 use MCommon;
 
-sub tokenize {
-  my ($line, $pos) = @_;
+require Exporter;
+@ISA = qw(Exporter);
 
-  my @ret = ();
+@EXPORT = qw(escape parse);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+sub escape_word {
+  my ($arg) = @_;
+  if ( $arg =~ /[^a-zA-Z0-9\-\.\_\/\:\+\@]/ ) {
+      return("'" . ($arg =~ s/'/'\\''/gr) . "'"); #/
+  } else {
+      return($arg);
+  }
 }
+
+sub escape {
+  return join(' ', map { escape_word($_); } @_);
+}
+
+sub parse {
+  my ($line, $pos) = @_;
+  $pos //= length $line;
+  my $this = {};
+
+  $this->{pos} = $pos;
+  $this->{tokens} = [ split(' ', $line) ];
+
+  bless $this, ShellHelper;
+  return $this;
+}
+
+
+sub rebuild {
+  my ($this) = @_;
+
+  return escape(@{$this->{tokens}});
+}
+
+
+
+1;
