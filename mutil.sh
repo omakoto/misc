e134a3a635ea2f1788dc5d5a852a448550f16d91
Makoto Onuki <omakoto@google.com>
2017-08-29 10:34:55 -0700

    latest

--- mutil.sh
+++ mutil.sh
@@ -293,7 +293,7 @@ wb() {
     if adb logcat -b main -d -s 'Zygote:E' | grep -q 'Exit zygote' ; then
       echo ""
       notify -f "!!! Detected reboot loop !!!"
-      logcat -d | grep -A 10 -P '(FATAL EXCEPTION IN SYSTEM PROCESS)'
+      logcat -d | android-grep-restart
       return 1
     fi
     sleep 0.5
