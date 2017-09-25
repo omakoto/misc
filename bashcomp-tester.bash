78cbda7aa3c11738f15c51fc498447afc8fa6dfb
Makoto Onuki <makoto.onuki@gmail.com>
2017-09-24 20:49:48 -0700

    latest

--- bashcomp-tester.bash
+++ bashcomp-tester.bash
@@ -3,31 +3,24 @@
 # Installs an empty "comptest" command with a debug completion.
 
 function comptest() {
-  : # Empty command
+  shescape "$@"
 }
 
 function __comptest_comp() {
-# COMP_CWORD
-# COMP_KEY
-# COMP_LINE
-# COMP_POINT
-# COMP_TYPE
-# COMP_WORDBREAKS
-# COMP_WORDS
-
-  cat-alt <(
-      for n in \
-          COMP_CWORD \
-          COMP_KEY \
-          COMP_LINE \
-          COMP_POINT \
-          COMP_TYPE \
-          COMP_WORDBREAKS \
-          COMP_WORDS \
-          ; do
-        declare -p "$n"
-      done
-  )
+  {
+    for n in \
+        COMP_CWORD \
+        COMP_KEY \
+        COMP_LINE \
+        COMP_POINT \
+        COMP_TYPE \
+        COMP_WORDBREAKS \
+        COMP_WORDS \
+        ; do
+      declare -p "$n"
+    done
+  } >/tmp/compin.txt
+  IFS=$'\n' COMPREPLY=($(cat /etc/compreply.txt 2>/dev/null))
 }
 
 complete -F __comptest_comp comptest
