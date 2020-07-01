c47a1edbe0e1f508666f78531964e8ed35c41065
Makoto Onuki <makoto.onuki@gmail.com>
2020-07-01 10:50:23 -0700

    add pi http root

---
+++ pi-www/README.md
@@ -0,0 +1,3 @@
+#
+
+See raspberry-pi-cheatsheet.md
---
+++ pi-www/restart.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Reboot</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head/>
+<body>
+<form method="post">
+  <button type="submit">Reboot</button>
+  <input type="hidden" name="mode" value="reboot">
+</form>
+<form method="post">
+  <button type="submit">Shutdown</button>
+  <input type="hidden" name="mode" value="shutdown">
+</form>
+<?php
+if ($_POST["mode"] == "reboot") {
+  echo "Rebooting...";
+  exec('sudo /sbin/reboot');
+}
+if ($_POST["mode"] == "reboot") {
+  echo "Shutting down...";
+  exec('sudo /sbin/shutdown -h now');
+}
+?>
+</body>
+</html>
