e4047d6b7fb8000b39f12c5b436ba230923fca36
Makoto Onuki <makoto.onuki@google.com>
2020-08-04 11:20:25 -0700

    latest

--- python-startup.py
+++ python-startup.py
@@ -2,3 +2,9 @@ import sys
 
 from matplotlib import pyplot as plt
 import numpy as np
+
+from tensorflow import keras
+from keras.datasets import mnist
+from keras.utils import np_utils, to_categorical
+from keras import models
+from keras import layers
