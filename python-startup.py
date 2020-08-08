5a42cc2ddbdd22e85bc7123b42965a7b2d6f4701
Makoto Onuki <makoto.onuki@google.com>
2020-08-08 16:16:06 -0700

    latest

--- python-startup.py
+++ python-startup.py
@@ -6,9 +6,9 @@ import re
 from matplotlib import pyplot as plt
 import numpy as np
 
-from tensorflow import keras
-from keras.datasets import mnist
-from keras.utils import np_utils, to_categorical
-from keras import models
-from keras import layers
-
+def ts():
+    from tensorflow import keras
+    from keras.datasets import mnist
+    from keras.utils import np_utils, to_categorical
+    from keras import models
+    from keras import layers
