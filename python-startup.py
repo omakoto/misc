0398b374ee6ca1971013f9d54637d8cab0491cdb
Makoto Onuki <makoto.onuki@google.com>
2020-08-08 20:53:40 -0700

    latest

--- python-startup.py
+++ python-startup.py
@@ -6,6 +6,10 @@ import re
 from matplotlib import pyplot as plt
 import numpy as np
 
+from IPython.display import display as d
+
+from calc import show_result as p
+
 def ts():
     from tensorflow import keras
     from keras.datasets import mnist
