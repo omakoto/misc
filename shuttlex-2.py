2aa8f62506b90c046ba7e1ede0ac002a1be211d5
Makoto Onuki <makoto.onuki@google.com>
2020-11-14 11:00:23 -0800

    change indent style

--- shuttlex-2.py
+++ shuttlex-2.py
@@ -20,21 +20,22 @@ DEFAULT_DEVICE_NAME = "Contour Design ShuttleXpress"
 
 notify2.init(NAME)
 
+
 class Remapper(BaseRemapper):
 
     def __init__(self, device_name_regex: str, match_non_keyboards=False, grab_devices=True,
-                 write_to_uinput=True, uinput_events: Optional[Dict[int, List[int]]] = None,
-                 global_lock_name: str = os.path.basename(sys.argv[0]), enable_debug=False):
+            write_to_uinput=True, uinput_events: Optional[Dict[int, List[int]]] = None,
+            global_lock_name: str = os.path.basename(sys.argv[0]), enable_debug=False):
         super().__init__(device_name_regex,
-                         match_non_keyboards = True,
-                         grab_devices = True,
-                         write_to_uinput = True,
-                         uinput_events = None,
-                         global_lock_name = NAME,
-                         enable_debug = enable_debug)
+            match_non_keyboards=True,
+            grab_devices=True,
+            write_to_uinput=True,
+            uinput_events=None,
+            global_lock_name=NAME,
+            enable_debug=enable_debug)
 
     def remap(self, device: evdev.InputDevice, events: List[evdev.InputEvent]
-              ) -> List[evdev.InputEvent]:
+    ) -> List[evdev.InputEvent]:
         return super().remap(device, events)
 
     def on_device_detected(self, devices: List[evdev.InputDevice]):
@@ -53,18 +54,17 @@ class Remapper(BaseRemapper):
 def main(args, description=NAME):
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-m', '--match-device-name', metavar='D', default=DEFAULT_DEVICE_NAME,
-                        help='Use devices matching this regex')
+        help='Use devices matching this regex')
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output')
 
     args = parser.parse_args(args)
-
 
     def do():
         # evdev will complain if the thread has no event loop set.
         asyncio.set_event_loop(asyncio.new_event_loop())
 
         main_loop(BaseRemapper(device_name_regex=args.match_device_name,
-                               enable_debug=args.debug))
+            enable_debug=args.debug))
 
     th = Thread(target=do)
     th.setDaemon(True)
