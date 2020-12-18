ffde52dca3bda682fa2af3de91d8aca677a8bbf7
Makoto Onuki <makoto.onuki@google.com>
2020-12-18 09:19:14 -0800

    latest

--- singleton.py
+++ singleton.py
@@ -6,7 +6,6 @@ import fcntl
 import os
 import time
 
-lockfiles = []
 
 def ensure_singleton(global_lock_name, debug=False):
     lockfile = f'/tmp/{global_lock_name}.lock'
@@ -16,7 +15,6 @@ def ensure_singleton(global_lock_name, debug=False):
         os.umask(0o000)
         lock = open(lockfile, 'w')
         fcntl.flock(lock, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        lockfiles.append(lock)
     except IOError:
         raise SystemExit(f'Unable to obtain file lock {lockfile}. Previous process running.')
 
