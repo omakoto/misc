1c9b07ecac97aae154adc96be9ce673aab52d0be
Makoto Onuki <makoto.onuki@google.com>
2021-10-10 17:21:17 -0700

    wip

--- visualizer-test.py
+++ visualizer-test.py
@@ -5,7 +5,7 @@ import os
 
 import pygame
 import pygame.midi
-import time
+from pprint import pprint
 
 
 def print_device_info():
@@ -38,71 +38,143 @@ def detect_input_device():
         print(f'Using device #{i}: {name}')
         return i
 
+    raise Exception('MIDI input device not found')
 
-def main():
-    pygame.init()
-    pygame.midi.init()
+# class Note:
+#     def __init__(self, ):
+#         self._velocity = 0
+#         self._timestamp = 0
 
-    pygame.fastevent.init()
-    event_get = pygame.fastevent.get
-    event_post = pygame.fastevent.post
+#     @property
+#     def velocity(self):
+#         return self._velocity
 
-    # print_device_info()
+#     @velocity.setter
+#     def velocity(self, value):
+#         self._velocity = value
 
-    input_id = detect_input_device()
-    midi_in = pygame.midi.Input(input_id)
+#     @property
+#     def timestamp(self):
+#         return self._timestamp
 
-    infoObject = pygame.display.Info()
+#     @timestamp.setter
+#     def timestamp(self, value):
+#         self._timestamp = value
 
-    screen_w = int(infoObject.current_w/2.5)
-    screen_h = int(infoObject.current_w/2.5)
+#     def __repr__(self):
+#         return f'{self._velocity} @{self._timestamp}'
 
-    screen = pygame.display.set_mode([screen_w, screen_h], pygame.RESIZABLE)
 
-    t = pygame.time.get_ticks()
-    getTicksLastFrame = t
 
-    running = True
-    while running:
+# class Model:
+#     def __init__(self, ):
+#         self.notes = [Note() for n in range(0, NOTES_COUNT)]
 
+
+#     def dump(self):
+#         pprint(vars(self))
+# Model().dump()
+# sys.exit(0)
+
+NOTES_COUNT = 128
+
+
+
+class Main:
+    def __init__(self, midi_input_id = None):
+        self.midi_input_id = midi_input_id
+        self.screen = None
+        self.initialized = False
+
+        # notes = [[0 or 1, velocity, timestamp], ....]
+        self.notes = [[0, 0, 0] for n in range(0, NOTES_COUNT)]
+
+
+    def init(self):
+        pygame.init()
+        pygame.midi.init()
+
+        pygame.fastevent.init()
+        self.event_get = pygame.fastevent.get
+        self.event_post = pygame.fastevent.post
+
+        # print_device_info()
+
+        if not self.midi_input_id:
+            self.midi_input_id = detect_input_device()
+        self.midi_in = pygame.midi.Input(self.midi_input_id)
+
+        infoObject = pygame.display.Info()
+
+        screen_w = int(infoObject.current_w/2.5)
+        screen_h = int(infoObject.current_w/2.5)
+
+        self.screen = pygame.display.set_mode([screen_w, screen_h], pygame.RESIZABLE)
+
+        self.initialized = True
+        return self
+
+
+    def __del__(self):
+        if self.initialized:
+            del self.midi_in
+            pygame.midi.quit()
+            pygame.quit()
+
+
+    def run(self):
         t = pygame.time.get_ticks()
-        deltaTime = (t - getTicksLastFrame) / 1000.0
-        getTicksLastFrame = t
 
-        # Did the user click the window close button?
-        for event in event_get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type in [pygame.midi.MIDIIN]:
-                print(event)
+        running = True
+        while running:
 
-        if midi_in.poll():
-            midi_events = midi_in.read(10)
-            # convert them into pygame events.
-            midi_evs = pygame.midi.midis2events(
-                midi_events, midi_in.device_id)
+            t = pygame.time.get_ticks()
 
-            for m_e in midi_evs:
-                event_post(m_e)
+            # Did the user click the window close button?
+            for event in self.event_get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type in [pygame.midi.MIDIIN]:
+                    print(event)
 
+                    if event.status == 144: # Note on
+                        self.notes[event.data1][0] = 1
+                        self.notes[event.data1][1] = event.data2
+                        self.notes[event.data1][2] = t
+                    elif event.status == 128:  # Note off
+                        self.notes[event.data1][0] = 0
+                        self.notes[event.data1][2] = t
+
+            if self.midi_in.poll():
+                midi_events = self.midi_in.read(10)
+                # convert them into pygame events.
+                midi_evs = pygame.midi.midis2events(
+                    midi_events, self.midi_in.device_id)
+
+                for m_e in midi_evs:
+                    self.event_post(m_e)
 
 # Key-on
 # <Event(32771-MidiIn {'status': 144, 'data1': 48, 'data2': 80, 'data3': 0, 'timestamp': 1111, 'vice_id': 3}) >
 # Key-off
 # <Event(32771-MidiIn {'status': 128, 'data1': 48, 'data2': 0, 'data3': 0, 'timestamp': 1364, 'vice_id': 3}) >
 
+            self._draw()
 
-        w = screen.get_width()
-        h = screen.get_height()
 
-        screen.fill((0, 0, 0))
+    def _draw(self):
+        w = self.screen.get_width()
+        h = self.screen.get_height()
+
+        self.screen.fill((0, 0, 0))
 
         # Flip the display
         pygame.display.flip()
 
-    del midi_in
-    pygame.midi.quit()
-    pygame.quit()
+
+def main():
+    m = Main().init()
+    m.run()
 
 
 if __name__ == '__main__':
