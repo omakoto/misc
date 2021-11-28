32dca2cb289f9d84a49df6c81010840e85a3788b
Makoto Onuki <makoto.onuki@google.com>
2021-11-28 15:30:45 -0800

    latest

--- clone-1t-backup-ssd.sh
+++ clone-1t-backup-ssd.sh
@@ -3,12 +3,9 @@
 set -e
 . mutil.sh
 
-if ! [[ -e /dev/mapper/luks-2ded3f73-866c-496e-9466-42ec30d400ad ]] || ! [[ -e /dev/mapper/luks-7db17b5b-fef7-483a-b83c-62d8afe0f684 ]] ; then
+if ! [[ -e /dev/mapper/luks-f304748c-610b-426e-ab51-b5fcbd4d337f ]] || ! [[ -e /dev/mapper/luks-157c47a2-4291-4a68-a76a-83cf46e76cbf ]] ; then
     echo 'Target or source disk not found. Make sure to decrypt them first.' 1>&2
     exit 1
 fi
 
-exit 0
-
-
-ee sudo partclone.ext4 -d -b -s /dev/mapper/luks-2ded3f73-866c-496e-9466-42ec30d400ad -o /dev/mapper/luks-7db17b5b-fef7-483a-b83c-62d8afe0f684
+ee sudo partclone.ext4 -d -b -s /dev/mapper/luks-f304748c-610b-426e-ab51-b5fcbd4d337f -o /dev/mapper/luks-157c47a2-4291-4a68-a76a-83cf46e76cbf
