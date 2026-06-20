0c0f8fbbdf18fbe75df5ee0225c04af2c777656e
Makoto Onuki <makoto.onuki@google.com>
2026-06-20 16:35:25 -0700

    Cleaning up bootstrap config

--- bootstrap-enc.sh
+++ bootstrap-enc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01d700d5fd57f35a869749a2dd3c132cbed08d4d55c941d3d9711c78306f739f
-size 26097
+oid sha256:6ca3ab464d658d13dad6a1567b2275488df48aa41ea131855aab20f252e6432c
+size 26014
