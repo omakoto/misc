68fe504ca6ac691ab58526801509e42e0cd9071e
Makoto Onuki <makoto.onuki@google.com>
2026-06-24 14:56:04 -0700

    switch to terminator

--- bootstrap-enc.sh
+++ bootstrap-enc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26ced8e529d9237060161d56535b76ca1dd44b9e8093594883c3e4f4c8de4e45
-size 25422
+oid sha256:4bb159d4f6510534fc58a36cdef4ed69a1ea5aba569cbcf35fc55c5c388b5485
+size 25457
