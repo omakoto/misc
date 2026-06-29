2b1490335de244d37cf3235e88aa943ec65b5e16
Makoto Onuki <makoto.onuki@google.com>
2026-06-29 10:55:52 -0700

    Remove unnecessary "time" use

--- mutil.sh
+++ mutil.sh
@@ -656,7 +656,7 @@ source-setup() {
       if [[ "$last_auto_sourced_script" != "$script" ]]; then
         INFO "Automatically running:" "$script"
         last_auto_sourced_script="$script"
-        time . "$script"
+        . "$script"
       fi
       return 0
     fi
