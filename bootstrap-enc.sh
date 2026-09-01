7bcf93bea8d56d68ca08570d1d163e16c24ceb3c
Makoto Onuki <makoto.onuki@google.com>
2026-08-31 20:02:13 -0700

    update bootstrap

--- bootstrap-enc.sh
+++ bootstrap-enc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94dde08171ce1df3ab6c60c5c0c37b6d8d41a8db4aea5f2f701eb1b9d605597c
-size 25786
+oid sha256:a52a72d17a12dd5d0c9e91dcb41fd5c731ee96aed302decf95d0999918884367
+size 25815
